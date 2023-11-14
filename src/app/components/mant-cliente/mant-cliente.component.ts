16132559b1e32e75364f44633bf146fc9d857989
HerberVillegasHuaman <herber.villegas.hu@gmail.com>
2023-11-13 23:25:33 -0500

    Complete Mantenimiento Clientes

--- src/app/components/mant-cliente/mant-cliente.component.ts
+++ src/app/components/mant-cliente/mant-cliente.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { ClienteService } from './../../services/cliente.service';
 import { clienteDto, clienteCreateDto, clienteUpdateDto, clienteDeleteDto } from 'src/app/models/cliente.types';
-
 
 @Component({
   selector: 'app-mant-cliente',
@@ -14,33 +13,30 @@ export class MantClienteComponent implements OnInit {
   public clients: any = [];
   public loading = false;
   public formCliente!: FormGroup;
- 
+  public valid = false;
 
   constructor(
     private clientService: ClienteService,
     private formBuilder: FormBuilder,
-   
-    ) {}
-  
-  ngOnInit(): void {
 
+  ) { }
+
+  ngOnInit(): void {
     this.formCliente = this.formBuilder.group({
       dni: ['', Validators.required],
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       direccion: ['', Validators.required],
       correo: ['', Validators.required],
-      // clave: [''],
       telefono: [''],
-      idCli:['']
+      idCli: ['']
     })
-
     this.getDataCliente();
   }
 
-   public limpiarForm(): void{
+  public limpiarForm(): void {
     const formCli = this.formCliente.value;
-    for(const cli in formCli){
+    for (const cli in formCli) {
       this.formCliente.get(cli)?.reset();
     }
   }
@@ -54,86 +50,66 @@ export class MantClienteComponent implements OnInit {
     )
   }
 
-  test(){
-    // let data = this.clients.map(
-    //   (clients:any)=>this.clients.dni)
-
-      console.log(this.formCliente.get('dni')?.value)
-      
-
-
-    // let validator = this.formCliente.get('dni')?.value
-    // for (let element of this.clients.dni){
-
-    // }
-    
-    
-  }
-
- async guardarUsuario() {
-
-  dni: this.formCliente.get('dni')?.value,
-
-    console.log(this.formCliente.value)
-    if(this.formCliente.valid){
-    const nuevoCliente: clienteCreateDto = {
-      dni: this.formCliente.get('dni')?.value,
-      nombre: this.formCliente.get('nombre')?.value,
-      apellido: this.formCliente.get('apellido')?.value,
-      direccion: this.formCliente.get('direccion')?.value,
-      correo: this.formCliente.get('correo')?.value,
-      // clave: this.formCliente.get('clave')?.value,
-      telefono: this.formCliente.get('telefono')?.value
-    };
-    this.clientService.createClient(nuevoCliente).subscribe(
-      (respuesta:clienteCreateDto) => {
-        console.log('Cliente creado exitosamente', respuesta);
-        this.getDataCliente();
-      },
-      (error) => {
-        console.error('Error al crear cliente', error);
-      }
-    );
-    console.log(this.formCliente.value)
-    this.limpiarForm();
-    setTimeout(()=>this.getDataCliente(),400) ;
+  async guardarUsuario() {
+    const ClientDni = this.clients.map((cliente: any) => cliente.dni)
+    for (let element of ClientDni) {
+      if (element == this.formCliente.get('dni')?.value) {
+        this.valid = false;
+      } null
+    }
+    if (this.formCliente.valid && this.valid == true) {
+      const nuevoCliente: clienteCreateDto = {
+        dni: this.formCliente.get('dni')?.value,
+        nombre: this.formCliente.get('nombre')?.value,
+        apellido: this.formCliente.get('apellido')?.value,
+        direccion: this.formCliente.get('direccion')?.value,
+        correo: this.formCliente.get('correo')?.value,
+        telefono: this.formCliente.get('telefono')?.value
+      };
+      this.clientService.createClient(nuevoCliente).subscribe(
+        (respuesta: clienteCreateDto) => {
+          console.log('Cliente creado exitosamente', respuesta);
+          this.getDataCliente();
+        },
+        (error) => {
+          console.error('Error al crear cliente', error);
+        }
+      );
+      this.limpiarForm();
+      setTimeout(() => this.getDataCliente(), 350);
     }
   }
 
-  async Actualizar(){
+  async Actualizar() {
     console.log(this.formCliente.valid);
-    if(this.formCliente.valid){
-    let datos:clienteUpdateDto = this.formCliente.value;
-    this.clientService.updateClient(datos).subscribe(
-      (data:clienteUpdateDto) => {
-        console.log('Actualizado', data);
-      },
-      (error) => {
-        console.error('Error al actualizar:', error);
-      }
-    );
-    this.limpiarForm();
-    setTimeout(()=>this.getDataCliente(),400) 
+    if (this.formCliente.valid) {
+      let datos: clienteUpdateDto = this.formCliente.value;
+      this.clientService.updateClient(datos).subscribe(
+        (data: clienteUpdateDto) => {
+          console.log('Actualizado', data);
+        },
+        (error) => {
+          console.error('Error al actualizar:', error);
+        }
+      );
+      this.limpiarForm();
+      setTimeout(() => this.getDataCliente(), 350)
     }
   }
 
-
-  public editarCliente(event:any){
+  public editarCliente(event: any) {
     console.log(event)
     this.formCliente.get('dni')?.setValue(event.dni),
-    this.formCliente.get('nombre')?.setValue(event.nombre),
-    this.formCliente.get('apellido')?.setValue(event.apellido),
-    this.formCliente.get('direccion')?.setValue(event.direccion),
-    this.formCliente.get('correo')?.setValue(event.correo),
-    // this.formCliente.get('clave')?.setValue(event.clave),
-    this.formCliente.get('telefono')?.setValue(event.telefono)
+      this.formCliente.get('nombre')?.setValue(event.nombre),
+      this.formCliente.get('apellido')?.setValue(event.apellido),
+      this.formCliente.get('direccion')?.setValue(event.direccion),
+      this.formCliente.get('correo')?.setValue(event.correo),
+      this.formCliente.get('telefono')?.setValue(event.telefono)
     this.formCliente.get('idCli')?.setValue(event.idCli)
   }
 
-  public eliminarCliente(cliente:any) {
-    
+  public eliminarCliente(cliente: any) {
     const confirmarEliminacion = confirm('¿Estás seguro de que quieres eliminar este cliente?');
-
     if (confirmarEliminacion) {
       this.clientService.deleteClient(cliente.idCli).subscribe(
         () => {
@@ -144,15 +120,6 @@ export class MantClienteComponent implements OnInit {
           console.error('Error al eliminar cliente', error);
         }
       );
-
-
-      }
     }
-
-      
-
-
-
-
-
+  }
 }
